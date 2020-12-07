b058eaee260fb4fc78a63fd19abd85ca63ddbcc1
marmbright <marmbright@verizon.net>
2020-12-07 18:53:17 -0500

    time to die

--- workshop-server/server.js
+++ workshop-server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const bodyParser = require("body-parser");
+
+app.set("port", 8080);
+
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("port", 8080);
+const Pool = require("pg").Pool
+const config = {
+	    host: process.env.DB_HOST,
+	    user: process.env.DB_USER,
+	    password: process.env.DB_PASS,
+	    database: "workshop"
+};
+const pool = new Pool(config)
 
 app.get('/hello', (req, res) => {
   // console log the request query json object
@@ -16,27 +26,28 @@ app.get('/hello', (req, res) => {
   res.json({response: `Hello, ${req.query.person}`});
 });
 app.post("/api", async(req,res) => {
-	        const workshop = req.body.workshop;
-	        const attendee = req.body.attendee;
-	        try{
-			
-			const template = "SELECT * FROM workshops WHERE workshop = $1 AND attendee = $2";
-	                const check = await pool.query(template, [workshop, attendee]);
-			if(check.rowCount != 0){
-				res.json({status: "attendee already in database"});
-			}else{
-				const template1 = "INSERT INTO workshops (workshop, attendee) VALUES ($1,$2)";
-				const response = await pool.query(template1, [workshop, attendee]);
-				res.json({status: "added"});
-			}
-		}catch (err){
-			console.log(err);
+	const workshop = req.body.workshop;
+        const attendee = req.body.attendee;
+	try{
+		
+		const template = "SELECT * FROM workshops WHERE workshop = $1 AND attendee = $2";
+		const check = await pool.query(template, [workshop, attendee]);
+		if(check.rowCount != 0){
+		res.json({status: "attendee already in database"});
+		}else{
+			const template1 = "INSERT INTO workshops (workshop, attendee) VALUES ($1,$2)";
+			const response = await pool.query(template1, [workshop, attendee]);
+			res.json({status: "added"});
 		}
+	}catch (err){
+		console.log(err);
+	}
 });
 
-app.get("/api", (req, res) => {
+app.get("/api", async(req, res) => {
 	const template = "SELECT * FROM workshops";
-	res.json(template);
+	const check = await pool.query(template, [workshop]);
+	res.json({status: "complete", result: {workshop: workshop}});
 });
 
 app.listen(app.get("port"), () => {
