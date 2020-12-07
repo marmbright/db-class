01369549819e564b2d7d6076f4f4cdafb8d466f4
marmbright <marmbright@verizon.net>
2020-12-07 15:26:49 -0500

    someone kill me please

--- workshop-server/server.js
+++ workshop-server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("port", 8080);
-console.log("Test");
 
 app.get('/hello', (req, res) => {
   // console log the request query json object
@@ -14,8 +15,29 @@ app.get('/hello', (req, res) => {
   // now send a response back to the client
   res.json({response: `Hello, ${req.query.person}`});
 });
+app.post("/api", async(req,res) => {
+	        const workshop = req.body.workshop;
+	        const attendee = req.body.attendee;
+	        try{
+			
+			const template = "SELECT * FROM workshops WHERE workshop = $1 AND attendee = $2";
+	                const check = await pool.query(template, [workshop, attendee]);
+			if(check.rowCount != 0){
+				res.json({status: "attendee already in database"});
+			}else{
+				const template1 = "INSERT INTO workshops (workshop, attendee) VALUES ($1,$2)";
+				const response = await pool.query(template1, [workshop, attendee]);
+				res.json({status: "added"});
+			}
+		}catch (err){
+			console.log(err);
+		}
+});
 
-
+app.get("/api", (req, res) => {
+	const template = "SELECT * FROM workshops";
+	res.json(template);
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); 
---
+++ workshop-server/ws1.sql
@@ -0,0 +1,11 @@
+\c workshop
+DROP DATABASE IF EXISTS workshop;
+CREATE DATABASE workshop;
+\c workshop
+CREATE TABLE workshops (
+	        id SERIAL PRIMARY KEY,
+		        workshop TEXT NOT NULL,
+			        attendee TEXT
+			);
+			GRANT SELECT, INSERT ON workshops TO marmbright;
+			GRANT USAGE, SELECT ON SEQUENCE workshops_id_seq TO marmbright;
